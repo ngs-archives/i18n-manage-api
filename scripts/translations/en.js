6c0a724799ad6c09b29226831425e8be6a47f85d
Atsushi NAGASE <atsn.ngs@gmail.com>
2015-03-29 04:12:15 +0900

    Generated translations by ngs via I18n Manager

--- scripts/translations/en.js
+++ scripts/translations/en.js
@@ -1,32 +1,6 @@
 angular.module('ngs.i18nManage.demo')
 .config(function($translateProvider) {
-  $translateProvider.translations('en', {
-    "title": "I18n Manager Demo",
-    "languages": {
-      "en": {
-        "title": "English"
-      },
-      "ja": {
-        "title": "日本語"
-      }
-    },
-    "introduction": {
-      "title": "Demo",
-      "paragraph1": "This is a demo application of <a href=\"https://github.com/ngs/i18n-manage-api\">i18n manager.",
-      "paragraph2": "Start inline translating by clicking element with <kbd>ctrl + shift</kbd> key."
-    },
-    "translationTable": {
-      "title": "Translations",
-      "diffOnlyButton": {
-        "title": "Show only diffs"
-      }
-    },
-    "pullRequest": {
-      "title": "Pull Request",
-      "login": "Login",
-      "selectFork": "Select fork",
-      "createFork": "Create fork",
-      "or": "or"
-    }
-  });
+  $translateProvider.translations('ja',{
+  "title": "I18n Manager Demo 2"
+});
 });
