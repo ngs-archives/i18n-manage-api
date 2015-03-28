b748351fc97170a7ea732e51293252cae92fe0ba
Atsushi NAGASE <atsn.ngs@gmail.com>
2015-03-29 06:21:39 +0900

    Generated translations by ngs via I18n Manager

--- scripts/translations/ja.js
+++ scripts/translations/ja.js
@@ -1,32 +1,32 @@
 angular.module('ngs.i18nManage.demo')
 .config(function($translateProvider) {
-  $translateProvider.translations('ja', /* begin:generatedData */{
-    "title": "I18n 管理デモ",
+  $translateProvider.translations('ja',/* begin:generatedData */{
+    "title": "I18n 管理デモ 2",
     "languages": {
-      "en": {
-        "title": "English"
-      },
-      "ja": {
-        "title": "日本語"
-      }
+        "en": {
+            "title": "English"
+        },
+        "ja": {
+            "title": "日本語"
+        }
     },
     "introduction": {
-      "title": "デモ",
-      "paragraph1": "これは <a href=\"https://github.com/ngs/i18n-manage-api\">i18n manager</a> のデモアプリケーションです。",
-      "paragraph2": "<kbd>ctrl + shift</kbd> キーを押しながら要素をクリックすることで、インライン翻訳が開始できます。"
+        "title": "デモ",
+        "paragraph1": "これは <a href=\"https://github.com/ngs/i18n-manage-api\">i18n manager</a> のデモアプリケーションです。",
+        "paragraph2": "<kbd>ctrl + shift</kbd> キーを押しながら要素をクリックすることで、インライン翻訳が開始できます。"
     },
     "translationTable": {
-      "title": "翻訳一覧",
-      "diffOnlyButton": {
-        "title": "差分のみ表示する"
-      }
+        "title": "翻訳一覧",
+        "diffOnlyButton": {
+            "title": "差分のみ表示する"
+        }
     },
     "pullRequest": {
-      "title": "プルリクエスト",
-      "login": "ログイン",
-      "selectFork": "フォークを選択",
-      "createFork": "フォークする",
-      "or": "または"
+        "title": "プルリクエスト",
+        "login": "ログイン",
+        "selectFork": "フォークを選択",
+        "createFork": "フォークする",
+        "or": "または"
     }
-  } /* end:generatedData */);
+}/* end:generatedData */);
 });
