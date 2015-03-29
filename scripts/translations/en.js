7bd002f49b20e9a393459acf42cb04e1fc38981a
Atsushi NAGASE <atsn.ngs@gmail.com>
2015-03-29 21:33:35 +0900

    Generated translations by ngs via I18n Manager

---
+++ scripts/translations/en.js
@@ -0,0 +1,9 @@
+angular.module('ngs.i18nManage.demo')
+.config(function($translateProvider) {
+  $translateProvider.translations('en',/* begin:generatedData */{
+    "introduction": {
+      "paragraph2": "Let's start inline translating by clicking element with <kbd>option (alt)</kbd> key."
+    },
+    "title": "I18n Manager Demo!"
+  }/* end:generatedData */);
+});
